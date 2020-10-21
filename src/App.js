04a2b99361b2a211896e995d2b11b08169ddcff4
Robin-Kevin Koppa <rokopp@ttu.ee>
2020-10-21 17:40:55 +0300

    Added app-bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AppBar from "./components/pages/navbar/AppBar";
 
 function App() {
   return (
     <div className="App">
+        <AppBar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
